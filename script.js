d5c234544a039c20017c4d396c87c6239a259392
hermann-wenninger <80582928+hermann-wenninger@users.noreply.github.com>
2021-06-25 09:31:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Show input error message
-function showError(input, message) {
+
+function showMeErrors(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
 }
 
-// Show success outline
-function showSuccess(input) {
+
+function showMeSuccesses(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-// Check email is valid
+
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
-    showError(input, 'Email is not valid');
+    showMeErrors(input, 'Email is not valid');
   }
 }
 
@@ -33,37 +33,37 @@ function checkRequired(inputArr) {
   let isRequired = false;
   inputArr.forEach(function(input) {
     if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+      showMeErrors(input, `${getFieldName(input)} is required`);
       isRequired = true;
     } else {
-      showSuccess(input);
+      showMeSuccesses(input);
     }
   });
 
   return isRequired;
 }
 
-// Check input length
+
 function checkLength(input, min, max) {
   if (input.value.length < min) {
-    showError(
+    showMeErrors(
       input,
       `${getFieldName(input)} must be at least ${min} characters`
     );
   } else if (input.value.length > max) {
-    showError(
+    showMeErrors(
       input,
       `${getFieldName(input)} must be less than ${max} characters`
     );
   } else {
-    showSuccess(input);
+    showMeSuccesses(input);
   }
 }
 
 // Check passwords match
 function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
+    showMeErrors(input2, 'Passwords do not match');
   }
 }
 
